787ecb45b81ee8ce179ab3326394a42a79f40189
ErineParan <82492409+ErineParan@users.noreply.github.com>
2021-04-14 13:23:12 +0800

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,34 @@
+## Project
+
+makeCacheMatrix <- function(x = matrix()) {
+
+invrs <- NULL
+set <- function(y) {
+x <- y
+invrs <- NULL
+}
+get <- function() x
+setinverse <- function(inverse) invrs <- inverse
+getinverse <- function() invrs
+list(set = set, get = get,
+setinverse = setinverse,
+getinverse = getinverse)
+}
+
+cacheSolve <- function(x, ...) {
+invrs <= x$getinverse()
+if(!is.null(invrs)) {
+message("getting cached data")
+return(invrs)
+}
+mat <- x$get()
+invrs <- solve(mat, ...)
+x$setinverse(invrs)
+invrs
+}
+
+## Math144 coursera project
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
